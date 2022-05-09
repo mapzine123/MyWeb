cc1daba6923546ebf265926ff7d0543b4c533ba0
un <66024439+mapzine123@users.noreply.github.com>
2022-05-09 15:48:32 +0900

    BoardService

--- src/main/java/com/firstSpring/app/service/BoardService.java
+++ src/main/java/com/firstSpring/app/service/BoardService.java
@@ -2,10 +2,12 @@ package com.firstSpring.app.service;
 
 import com.firstSpring.app.dao.BoardDao;
 import com.firstSpring.app.domain.BoardDto;
+import com.firstSpring.app.domain.SearchCondition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class BoardService {
@@ -18,5 +20,21 @@ public class BoardService {
 
     public List<BoardDto> viewBoardList() throws Exception {
         return dao.selectAll();
+    }
+
+    public List<BoardDto> getSearchResultPage(SearchCondition sc) throws Exception {
+        return dao.searchSelectPage(sc);
+    }
+
+    public int clearBoard() throws Exception {
+        return dao.deleteAll();
+    }
+
+    public int getCount() throws Exception {
+        return dao.getCount();
+    }
+
+    public int deleteBoard(Map map) throws Exception {
+        return dao.delete(map);
     }
 }
